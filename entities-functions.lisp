e81cc7515526a7fdf7afe2af6333c1bfe925d74c
Thomas Bakketun <thomas@bakketun.pro>
2020-11-20 13:45:23 +0100

    Add full name in entity-trie-node to aid in debugging.

--- entities-functions.lisp
+++ entities-functions.lisp
@@ -36,13 +36,14 @@
 
 
 (defstruct entity-trie-node
-  prefix
+  (prefix "")
+  char
   code-points
   (subnodes (make-array 0)))
 
 
 (defun entity-trie-node-search (node char)
-  (find char (entity-trie-node-subnodes node) :key #'entity-trie-node-prefix))
+  (find char (entity-trie-node-subnodes node) :key #'entity-trie-node-char))
 
 
 (defun make-entities-trie (entities)
@@ -54,33 +55,29 @@
     root-node))
 
 
-(defun entity-trie-node-insert (node char-list code-points)
-  (destructuring-bind (prefix . suffix) char-list
-    (let ((subnode (find prefix (entity-trie-node-subnodes node) :key #'entity-trie-node-prefix)))
+(defun entity-trie-node-insert (node char-list code-points &optional prefix)
+  (destructuring-bind (char . suffix) char-list
+    (let ((subnode (find char (entity-trie-node-subnodes node) :key #'entity-trie-node-char)))
       (unless subnode
-        (setf subnode (make-entity-trie-node :prefix prefix))
+        (setf subnode (make-entity-trie-node :char char
+                                             :prefix (coerce (reverse prefix) 'string)))
         (setf (entity-trie-node-subnodes node) (concatenate 'vector
                                                             (entity-trie-node-subnodes node)
                                                             (make-array 1 :initial-element subnode))))
       (if suffix
-          (entity-trie-node-insert subnode suffix code-points)
+          (entity-trie-node-insert subnode suffix code-points (cons char prefix))
           (when code-points
             (setf (entity-trie-node-code-points subnode) code-points))))))
 
 
 (defmethod print-object ((node entity-trie-node) stream)
-  (flet ((print-it ()
-           (princ "(" stream)
-           (princ (entity-trie-node-prefix node) stream)
-           (when (entity-trie-node-code-points node)
-             (format stream "→~A" (entity-trie-node-code-points node)))
-           (loop :for subnode :across (entity-trie-node-subnodes node)
-                 :do (format stream "~A" subnode))
-           (princ ")" stream)))
-    (if *print-escape*
-        (print-unreadable-object (node stream :type t)
-          (print-it))
-        (print-it))))
+  (print-unreadable-object (node stream :type t)
+    (format stream "~A~A~:[~;…~]~@[=~S~]~[~:;~:* (~A)~]"
+            (entity-trie-node-prefix node)
+            (or (entity-trie-node-char node) "")
+            (plusp (length (entity-trie-node-subnodes node)))
+            (entity-trie-node-code-points node)
+            (length (entity-trie-node-subnodes node)))))
 
 
 (defparameter *entity-trie-root* (make-entities-trie *entities*))
