6bdc40cd7a0e6b5096e8248aec99a662bb68c9df
Thomas Bakketun <thomas@bakketun.pro>
2020-12-02 18:36:54 +0100

    interface/unicode-constants, don't repeat all the constants.

--- interface/unicode-constants.lisp
+++ interface/unicode-constants.lisp
@@ -18,50 +18,9 @@
 ;;;;  You should have received a copy of the GNU General Public License
 ;;;;  along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-(defpackage #:html5-parser/interface/unicode-constants
+(uiop:define-package #:html5-parser/interface/unicode-constants
   (:export
-   #:U+0000_NULL
-   #:U+0009_CHARACTER_TABULATION
-   #:U+000A_LINE_FEED
-   #:U+000C_FORM_FEED
-   #:U+000D_CARRIAGE_RETURN
-   #:U+0020_SPACE
-   #:U+0021_EXCLAMATION_MARK_!
-   #:U+0022_QUOTATION_MARK_\"
-   #:U+0023_NUMBER_SIGN_\#
-   #:U+0026_AMPERSAND_&
-   #:U+0027_APOSTROPHE_\'
-   #:U+002D_HYPHEN-MINUS_-
-   #:U+002F_SOLIDUS_/
-   #:U+003B_SEMICOLON_\;
-   #:U+003C_LESS-THAN_SIGN_<
-   #:U+003D_EQUALS_SIGN_=
-   #:U+003E_GREATER-THAN_SIGN_>
-   #:U+003F_QUESTION_MARK_?
-   #:U+0058_LATIN_CAPITAL_LETTER_X
-   #:U+005B_LEFT_SQUARE_BRACKET_[
-   #:U+005D_RIGHT_SQUARE_BRACKET_]
-   #:U+0060_GRAVE_ACCENT_\`
-   #:U+0041_LATIN_CAPITAL_LETTER_A
-   #:U+0043_LATIN_CAPITAL_LETTER_C
-   #:U+0044_LATIN_CAPITAL_LETTER_D
-   #:U+0054_LATIN_CAPITAL_LETTER_T
-   #:U+0061_LATIN_SMALL_LETTER_A
-   #:U+0062_LATIN_SMALL_LETTER_B
-   #:U+0063_LATIN_SMALL_LETTER_C
-   #:U+0064_LATIN_SMALL_LETTER_D
-   #:U+0065_LATIN_SMALL_LETTER_E
-   #:U+0069_LATIN_SMALL_LETTER_I
-   #:U+006C_LATIN_SMALL_LETTER_L
-   #:U+006D_LATIN_SMALL_LETTER_M
-   #:U+006F_LATIN_SMALL_LETTER_O
-   #:U+0070_LATIN_SMALL_LETTER_P
-   #:U+0073_LATIN_SMALL_LETTER_S
-   #:U+0074_LATIN_SMALL_LETTER_T
-   #:U+0075_LATIN_SMALL_LETTER_U
-   #:U+0078_LATIN_SMALL_LETTER_X
-   #:U+0079_LATIN_SMALL_LETTER_Y
-   #:U+FFFD_REPLACEMENT_CHARACTER
+   ;; All the define-unicode-constants below
    )
   (:use
    #:common-lisp
@@ -73,7 +32,9 @@
   (let* ((code-point (symbol-name symbol)))
     (assert (eql 0 (search "U+" code-point)))
     (let ((char (code-char (parse-integer code-point :start 2 :radix 16 :junk-allowed t))))
-      `(defconstant ,symbol ,char))))
+      `(progn
+         (export ',symbol)
+         (defconstant ,symbol ,char)))))
 
 
 (define-unicode-constant U+0000_NULL)
